556acbf4e7e2a9cbe2c46ebb35e63bfa812defba
Edrop7 <samovalle878@gmail.com>
2025-03-14 23:49:53 +0000

    tweaked speeds to successfully operate physical hardware robot remotely in Real Robot Lab (The Construct Sim, Barcelona, Spain)

--- section_1_topics/scripts/navigate_track.py
+++ section_1_topics/scripts/navigate_track.py
@@ -22,20 +22,20 @@ def take_action(laser_reading):
     # added a margin of 0.05 m vs spec from notebook to encompass robot width
     if laser_reading['front'] < 0.55:
         state_description = 'case 1 - corner reached, turn with radius 0.25'
-        linear_x = 0.1
-        angular_z = 0.75
+        linear_x = 0.075
+        angular_z = 1
     elif laser_reading['front'] > 0.55:
         if laser_reading['right'] < 0.25:
             state_description = 'case 2 - too close to right wall'
-            linear_x = 0.1
-            angular_z = 0.2
+            linear_x = 0.075
+            angular_z = 0.25
         elif laser_reading['right'] > 0.25:
             state_description = 'case 3 - too far from right wall'
-            linear_x = 0.1
-            angular_z = -0.2
+            linear_x = 0.075
+            angular_z = -0.25
         else:
             state_description = 'case 4 - move straight'
-            linear_x = 0.1
+            linear_x = 0.075
             angular_z = 0.0
     else:
         state_description = 'unknown case'
@@ -52,9 +52,14 @@ def main():
 
     pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
     sub = rospy.Subscriber('/scan', LaserScan, callback_laser)
+
     rate = rospy.Rate(10) # 10hz
     rate.sleep()
-    rospy.spin()
+
+    rospy.spin()  
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
