e5f3171771dc9f57a9f7f46a9c5b08b8220ea457
Edrop7 <samovalle878@gmail.com>
2025-03-14 22:59:24 +0000

    finished section_1_topics package with successfully tested script to navigate turtlebot along track

--- scan_test/src/scan_test.py
+++ scan_test/src/scan_test.py
@@ -5,10 +5,10 @@ from sensor_msgs.msg import LaserScan
 
 def callback(msg):
     # print(len(msg.ranges))
-    print(f"right: {msg.ranges[359]}")
-    print(f"front: {msg.ranges[179]}")
-    print(f"left: {msg.ranges[0]}")
-    print(f"back: {msg.ranges[539]}")
+    print(f"right: {msg.ranges[179]}")
+    print(f"front: {msg.ranges[359]}")
+    print(f"left: {msg.ranges[539]}")
+    print(f"back: {msg.ranges[0]}")
 rospy.init_node('scan_values')
 sub = rospy.Subscriber('/scan', LaserScan, callback)
 rospy.spin()
--- section_1_topics/scripts/navigate_track.py
+++ section_1_topics/scripts/navigate_track.py
@@ -1,40 +1,42 @@
 #! /usr/bin/env python
 
 import rospy
-from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
 
-def clbk_laser(msg):
-    regions = {
-        'right':  min(msg.ranges[0]),
-        'front':  min(msg.ranges[359]),
+def callback_laser(msg):
+    laser_reading = {
+        # ray angles for right and front given min_angle = -pi radian, 720 indexes, right = 0 radian convention
+        'right': msg.ranges[179],
+        'front': msg.ranges[359]
     }
-    take_action(regions)
-    print('X: %s' % (str(regions)))
+    take_action(laser_reading)
+    print('X: %s' % (str(laser_reading)))
 
-def take_action(regions):
+def take_action(laser_reading):
     msg = Twist()
     linear_x = 0
     angular_z = 0
-
     state_description = ''
 
-    if regions['front'] >= 1:
-        state_description = 'case 1 - forward'
-        linear_x = 0.2
-        angular_z = 0
-    elif regions['front'] < 1:
-        state_description = 'case 2 - avoid front object'
-        linear_x = 0.0
-        angular_z = 1.0
-    elif regions['right'] < 1:
-        state_description = 'case 3 - avoid right object'
-        linear_x = 0.0
-        angular_z = 1.0
-    elif regions['left'] < 1:
-        state_description = 'case 4 - avoid left object'
-        linear_x = 0.0
-        angular_z = -1.0
+    # added a margin of 0.05 m vs spec from notebook to encompass robot width
+    if laser_reading['front'] < 0.55:
+        state_description = 'case 1 - corner reached, turn with radius 0.25'
+        linear_x = 0.1
+        angular_z = 0.75
+    elif laser_reading['front'] > 0.55:
+        if laser_reading['right'] < 0.25:
+            state_description = 'case 2 - too close to right wall'
+            linear_x = 0.1
+            angular_z = 0.2
+        elif laser_reading['right'] > 0.25:
+            state_description = 'case 3 - too far from right wall'
+            linear_x = 0.1
+            angular_z = -0.2
+        else:
+            state_description = 'case 4 - move straight'
+            linear_x = 0.1
+            angular_z = 0.0
     else:
         state_description = 'unknown case'
 
@@ -48,9 +50,10 @@ def main():
 
     rospy.init_node('topics_quiz_node', anonymous=True)
 
-    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
-    sub = rospy.Subscriber('/kobuki/laser/scan', LaserScan, clbk_laser)
-
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+    sub = rospy.Subscriber('/scan', LaserScan, callback_laser)
+    rate = rospy.Rate(10) # 10hz
+    rate.sleep()
     rospy.spin()
 
 if __name__ == '__main__':
