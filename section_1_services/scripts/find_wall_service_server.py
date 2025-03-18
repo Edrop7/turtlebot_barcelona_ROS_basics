c6064f503d8610f046e3ca63d3aed0c6d6949a44
Edrop7 <samovalle878@gmail.com>
2025-03-18 18:59:55 +0000

    move_robot_to_wall script for service completed and tested in simulator successfully

--- section_1_services/scripts/find_wall_service_server.py
+++ section_1_services/scripts/find_wall_service_server.py
@@ -9,11 +9,11 @@ def rotate_robot_to_wall():
     rospy.loginfo("Initiating turn to closest wall")
 
     lidar.get_min_ray()
+    msg = Twist()
 
     while not (lidar.min_ray['index'] < 363 and lidar.min_ray['index'] > 355):
         lidar.get_min_ray()
         # lidar.read_min_ray()
-        msg = Twist()
         msg.angular.z = 0.2
         pub.publish(msg)
     
@@ -24,11 +24,31 @@ def rotate_robot_to_wall():
 
     return None
 
+def move_robot_to_wall():
+    rospy.loginfo("Initiating approach to front wall")
+
+    lidar.get_cardinal_rays()
+    msg = Twist()
+
+    while lidar.cardinal_rays['front'] > 0.3:
+        # lidar.read_cardinal_rays()
+        msg.linear.x = 0.1
+        pub.publish(msg)
+        lidar.get_cardinal_rays()
+
+    msg.linear.x = 0.0
+    pub.publish(msg)
+
+    rospy.loginfo("Front wall within 30 cm of robot")
+        
+    return None
+
 def callback_service(request):
     lidar.get_min_ray()
     lidar.read_min_ray()
 
     rotate_robot_to_wall()
+    move_robot_to_wall()
 
     response = FindWallResponse()
     response.wallfound = True
